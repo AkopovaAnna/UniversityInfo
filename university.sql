f56c08eab283b371546b991043e9beb58ae125c9
AkopovaAnna <anna.akopova.1989@gmail.com>
2016-09-16 00:23:12 +0400

    changes in sql db tables

--- university.sql
+++ university.sql
@@ -16,141 +16,111 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`UniversityInfo` /*!40100 DEFAULT CHARAC
 
 USE `UniversityInfo`;
 
-/*Table structure for table `University` */
-
-DROP TABLE IF EXISTS `University`;
-
-CREATE TABLE `University` (
-  `uid` int(11) NOT NULL AUTO_INCREMENT,
-  `uname` char(40) COLLATE utf8_bin NOT NULL,
-  `city` char(40) COLLATE utf8_bin DEFAULT NULL,
-  `country` char(40) COLLATE utf8_bin DEFAULT NULL,
-  `address` char(40) COLLATE utf8_bin DEFAULT NULL,
-  `email` char(40) COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-/*Data for the table `University` */
-
 /*Table structure for table `course` */
 
 DROP TABLE IF EXISTS `course`;
 
 CREATE TABLE `course` (
-  `cid` int(11) NOT NULL AUTO_INCREMENT,
-  `cnum` char(40) COLLATE utf8_bin DEFAULT NULL,
-  `cname` varchar(250) COLLATE utf8_bin DEFAULT NULL,
-  `cunits` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `num` char(40) COLLATE utf8_bin DEFAULT NULL,
+  `name` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `units` int(11) DEFAULT NULL,
   `hour` time DEFAULT NULL,
-  `fid` int(11) DEFAULT NULL,
-  PRIMARY KEY (`cid`),
-  UNIQUE KEY `cnum` (`cnum`),
-  KEY `fid` (`fid`),
-  CONSTRAINT `course_ibfk_1` FOREIGN KEY (`fid`) REFERENCES `faculty` (`fid`)
+  `f_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `cnum` (`num`),
+  KEY `fid` (`f_id`),
+  CONSTRAINT `course_ibfk_1` FOREIGN KEY (`f_id`) REFERENCES `faculty` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-/*Data for the table `course` */
 
 /*Table structure for table `department` */
 
 DROP TABLE IF EXISTS `department`;
 
 CREATE TABLE `department` (
-  `did` int(11) NOT NULL AUTO_INCREMENT,
-  `dname` char(40) COLLATE utf8_bin DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` char(40) COLLATE utf8_bin DEFAULT NULL,
   `office` char(40) COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY (`did`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-/*Data for the table `department` */
 
 /*Table structure for table `enrolled` */
 
 DROP TABLE IF EXISTS `enrolled`;
 
 CREATE TABLE `enrolled` (
-  `eid` int(11) NOT NULL AUTO_INCREMENT,
-  `sid` int(11) DEFAULT NULL,
-  `cid` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `s_id` int(11) DEFAULT NULL,
+  `c_id` int(11) DEFAULT NULL,
   `grade` double DEFAULT NULL,
-  PRIMARY KEY (`eid`),
-  KEY `sid` (`sid`),
-  KEY `cid` (`cid`),
-  CONSTRAINT `enrolled_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `student` (`sid`),
-  CONSTRAINT `enrolled_ibfk_2` FOREIGN KEY (`cid`) REFERENCES `course` (`cid`)
+  PRIMARY KEY (`id`),
+  KEY `sid` (`s_id`),
+  KEY `cid` (`c_id`),
+  CONSTRAINT `enrolled_ibfk_1` FOREIGN KEY (`s_id`) REFERENCES `student` (`id`),
+  CONSTRAINT `enrolled_ibfk_2` FOREIGN KEY (`c_id`) REFERENCES `course` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-/*Data for the table `enrolled` */
 
 /*Table structure for table `faculty` */
 
 DROP TABLE IF EXISTS `faculty`;
 
 CREATE TABLE `faculty` (
-  `fid` int(11) NOT NULL AUTO_INCREMENT,
-  `fname` char(250) COLLATE utf8_bin DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` char(250) COLLATE utf8_bin DEFAULT NULL,
   `fee` bigint(20) DEFAULT NULL,
-  `did` int(11) DEFAULT NULL,
-  PRIMARY KEY (`fid`),
-  KEY `did` (`did`),
-  CONSTRAINT `faculty_ibfk_1` FOREIGN KEY (`did`) REFERENCES `department` (`did`) ON DELETE CASCADE ON UPDATE CASCADE
+  `d_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `did` (`d_id`),
+  CONSTRAINT `faculty_ibfk_1` FOREIGN KEY (`d_id`) REFERENCES `department` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-/*Data for the table `faculty` */
 
 /*Table structure for table `grades` */
 
 DROP TABLE IF EXISTS `grades`;
 
 CREATE TABLE `grades` (
-  `gid` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `grade` char(10) COLLATE utf8_bin DEFAULT NULL,
   `point` int(11) DEFAULT NULL,
-  PRIMARY KEY (`gid`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-/*Data for the table `grades` */
 
 /*Table structure for table `role` */
 
 DROP TABLE IF EXISTS `role`;
 
 CREATE TABLE `role` (
-  `rid` int(11) NOT NULL AUTO_INCREMENT,
-  `roletitle` varchar(250) COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY (`rid`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-/*Data for the table `role` */
 
 /*Table structure for table `section` */
 
 DROP TABLE IF EXISTS `section`;
 
 CREATE TABLE `section` (
-  `secid` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `semester` varchar(250) COLLATE utf8_bin DEFAULT NULL,
   `room` varchar(250) COLLATE utf8_bin DEFAULT NULL,
   `time` datetime DEFAULT NULL,
-  `cid` int(11) DEFAULT NULL,
-  `staffid` int(11) DEFAULT NULL,
-  PRIMARY KEY (`secid`),
-  KEY `cid` (`cid`),
-  KEY `staffid` (`staffid`),
-  CONSTRAINT `section_ibfk_1` FOREIGN KEY (`cid`) REFERENCES `course` (`cid`),
-  CONSTRAINT `section_ibfk_2` FOREIGN KEY (`staffid`) REFERENCES `staff` (`staffid`)
+  `c_id` int(11) DEFAULT NULL,
+  `staff_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `cid` (`c_id`),
+  KEY `staffid` (`staff_id`),
+  CONSTRAINT `section_ibfk_1` FOREIGN KEY (`c_id`) REFERENCES `course` (`id`),
+  CONSTRAINT `section_ibfk_2` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-/*Data for the table `section` */
 
 /*Table structure for table `staff` */
 
 DROP TABLE IF EXISTS `staff`;
 
 CREATE TABLE `staff` (
-  `staffid` int(11) NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(250) COLLATE utf8_bin DEFAULT NULL,
-  `lastname` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `last_name` varchar(250) COLLATE utf8_bin DEFAULT NULL,
   `surname` varchar(250) COLLATE utf8_bin DEFAULT NULL,
   `gender` char(10) COLLATE utf8_bin DEFAULT NULL,
   `birthdate` date DEFAULT NULL,
@@ -160,59 +130,67 @@ CREATE TABLE `staff` (
   `email` varchar(250) COLLATE utf8_bin DEFAULT NULL,
   `address` varchar(250) COLLATE utf8_bin DEFAULT NULL,
   `hiredate` date DEFAULT NULL,
-  `cid` int(11) DEFAULT NULL,
-  PRIMARY KEY (`staffid`),
+  `d_id` int(11) DEFAULT NULL,
+  `r_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
   UNIQUE KEY `ssn` (`ssn`),
-  KEY `cid` (`cid`),
-  CONSTRAINT `staff_ibfk_1` FOREIGN KEY (`cid`) REFERENCES `course` (`cid`) ON UPDATE CASCADE
+  KEY `d_id` (`d_id`),
+  KEY `r_id` (`r_id`),
+  CONSTRAINT `staff_ibfk_2` FOREIGN KEY (`d_id`) REFERENCES `department` (`id`),
+  CONSTRAINT `staff_ibfk_3` FOREIGN KEY (`r_id`) REFERENCES `role` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-/*Data for the table `staff` */
 
 /*Table structure for table `student` */
 
 DROP TABLE IF EXISTS `student`;
 
 CREATE TABLE `student` (
-  `sid` int(11) NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(250) COLLATE utf8_bin DEFAULT NULL,
-  `lastname` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `last_name` varchar(250) COLLATE utf8_bin DEFAULT NULL,
   `surname` varchar(250) COLLATE utf8_bin DEFAULT NULL,
   `birthdate` date DEFAULT NULL,
   `gender` char(10) COLLATE utf8_bin DEFAULT NULL,
   `emaiil` varchar(250) COLLATE utf8_bin DEFAULT NULL,
   `ssn` varchar(250) COLLATE utf8_bin DEFAULT NULL,
   `address` varchar(250) COLLATE utf8_bin DEFAULT NULL,
-  `studentnum` bigint(20) DEFAULT NULL,
+  `num` bigint(20) DEFAULT NULL,
   `enrolldate` date DEFAULT NULL,
   `degree` char(40) COLLATE utf8_bin DEFAULT NULL,
-  `did` int(11) DEFAULT NULL,
-  PRIMARY KEY (`sid`),
-  KEY `did` (`did`),
-  CONSTRAINT `student_ibfk_1` FOREIGN KEY (`did`) REFERENCES `department` (`did`)
+  `d_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `did` (`d_id`),
+  CONSTRAINT `student_ibfk_1` FOREIGN KEY (`d_id`) REFERENCES `department` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-/*Data for the table `student` */
 
 /*Table structure for table `teach` */
 
 DROP TABLE IF EXISTS `teach`;
 
 CREATE TABLE `teach` (
-  `tid` int(11) NOT NULL AUTO_INCREMENT,
-  `staffid` int(11) DEFAULT NULL,
-  `cid` int(11) DEFAULT NULL,
-  `rid` int(11) DEFAULT NULL,
-  PRIMARY KEY (`tid`),
-  KEY `staffid` (`staffid`),
-  KEY `cid` (`cid`),
-  KEY `rid` (`rid`),
-  CONSTRAINT `teach_ibfk_1` FOREIGN KEY (`staffid`) REFERENCES `staff` (`staffid`),
-  CONSTRAINT `teach_ibfk_2` FOREIGN KEY (`cid`) REFERENCES `course` (`cid`),
-  CONSTRAINT `teach_ibfk_3` FOREIGN KEY (`rid`) REFERENCES `role` (`rid`)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `staff_id` int(11) DEFAULT NULL,
+  `c_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `staffid` (`staff_id`),
+  KEY `cid` (`c_id`),
+  CONSTRAINT `teach_ibfk_1` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`id`),
+  CONSTRAINT `teach_ibfk_2` FOREIGN KEY (`c_id`) REFERENCES `course` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-/*Data for the table `teach` */
+/*Table structure for table `university` */
+
+DROP TABLE IF EXISTS `university`;
+
+CREATE TABLE `university` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) COLLATE utf8_bin NOT NULL,
+  `city` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `country` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `address` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  `email` varchar(250) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
